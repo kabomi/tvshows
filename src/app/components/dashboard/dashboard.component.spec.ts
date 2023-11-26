ba16c3666a1546c0b0d1b7c74e8f31d214080219
Imobach Martín <martin@365farmnet.com>
2023-11-26 10:12:15 +0000

    Dashboard should contain a row per genre

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -2,23 +2,46 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { DashboardComponent } from './dashboard.component';
+import { By } from '@angular/platform-browser';
+import { Component, Input } from '@angular/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  @Component({
+    imports: [DashboardComponent],
+    standalone: true,
+    template: `<app-dashboard [genres]="genres"></app-dashboard>`,
+  })
+  class TestHostComponent {
+    @Input()
+    genres: string[] = [];
+  }
 
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.autoDetectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should contain a grid', () => {
+    const element = fixture.debugElement.query(By.css('.dashboard-grid')).nativeElement as HTMLElement;
+    expect(element).toBeTruthy();
+  });
+  it("should contain a row per genre", () => {
+    const genres = ['suspense','comedy','fantasy','drama'];
+    const hostFixture = TestBed.createComponent(TestHostComponent);
+    const hostComponent = hostFixture.componentInstance;
+    hostComponent.genres = genres;
+    hostFixture.detectChanges();
+    const elements = hostFixture.debugElement.queryAll(By.css('.dashboard-genre-row'));
+    
+    expect(elements).toHaveSize(genres.length);
+  });
+
+  
 });
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  imports: [IonGrid, IonRow, IonCol, CommonModule]
 })
 export class DashboardComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  @Input()
+  genres: string[] = [];
+
+  trackByFn: (id: number, name: string) => string = (id, name) => name
+
+  ngOnInit() {
+    console.log("Dashboard onInit");
+  }
 
 }
