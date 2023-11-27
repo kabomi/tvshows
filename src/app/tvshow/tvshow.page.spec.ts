8586ca8c1bec354dc1795e56895d9cef6d5238df
Imobach Martín <martin@365farmnet.com>
2023-11-27 17:41:57 +0000

    Add TvShow Item component and a loading indicator

--- src/app/tvshow/tvshow.page.spec.ts
+++ src/app/tvshow/tvshow.page.spec.ts
@@ -1,11 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { TvshowPage } from './tvshow.page';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, RouterState } from '@angular/router';
+import { harryPotterMovie } from '../services/tvshows.mocks';
+import { covertTvShowResponse } from '../services/tvshows.model';
+import { By } from '@angular/platform-browser';
+import { TvshowItemComponent } from '../components/tvshow-item/tvshow-item.component';
 
 describe('TvshowPage', () => {
   let component: TvshowPage;
   let fixture: ComponentFixture<TvshowPage>;
+  let routerState: RouterState;
+
+  afterEach(async () => {
+    TestBed.resetTestingModule();
+    fixture.destroy();
+  });
 
   beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      // providers: [RouterState],
+      imports: [RouterTestingModule],
+    });
+    // routerState = TestBed.inject(RouterState);
     fixture = TestBed.createComponent(TvshowPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -13,5 +30,14 @@ describe('TvshowPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('shows a TvShowItem when "show" property is set', () => {
+    component.show = covertTvShowResponse(harryPotterMovie);
+    fixture.detectChanges();
+
+    const tvShowItem = fixture.debugElement.query(By.directive(TvshowItemComponent))
+      .componentInstance as TvshowItemComponent;
+    expect(tvShowItem).toBeTruthy();
   });
 });
--- src/app/tvshow/tvshow.page.ts
+++ src/app/tvshow/tvshow.page.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, LoadingController } from '@ionic/angular';
+import { TvShow, covertTvShowResponse } from '../services/tvshows.model';
+import { TvshowItemComponent } from '../components/tvshow-item/tvshow-item.component';
+import { harryPotterMovie } from '../services/tvshows.mocks';
 
 @Component({
   selector: 'app-tvshow',
   templateUrl: './tvshow.page.html',
   styleUrls: ['./tvshow.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, TvshowItemComponent],
 })
-export class TvshowPage implements OnInit {
-  constructor() {}
+export class TvshowPage implements AfterContentInit {
+  @Input()
+  show: TvShow | undefined = undefined;
 
-  ngOnInit() {
-    return;
+  constructor(private loadingCtrl: LoadingController) {}
+
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading Movie...',
+    });
+
+    loading.present();
+  }
+
+  ngAfterContentInit() {
+    this.showLoading();
   }
 }
