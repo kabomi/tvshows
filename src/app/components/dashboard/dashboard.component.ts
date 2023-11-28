424302ad4554c05cdf6fbd5ac0462e4e4f5707e0
Imobach Martín <martin@365farmnet.com>
2023-11-28 13:34:39 +0000

    Add swiper as suggested on ionic documentation

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input, OnInit } from '@angular/core';
 import { IonGrid, IonRow, IonCol, IonTitle } from '@ionic/angular/standalone';
 import { TvShow } from 'src/app/services/tvshows.model';
 import { TvshowItemComponent } from '../tvshow-item/tvshow-item.component';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [IonGrid, IonRow, IonCol, CommonModule, IonTitle, TvshowItemComponent],
 })
 export class DashboardComponent implements OnInit {
